8e73b6773376041615e3dff0f4a7979c51b37315
meetshah1995 <meetshah1995@iitb.ac.in>
2016-03-20 23:39:24 +0530

    Add q2 report final

--- 120050087_120050041_13D070003_assignment3/2/code/mainScript.m
+++ 120050087_120050041_13D070003_assignment3/2/code/mainScript.m
@@ -9,8 +9,8 @@ figure; imshow(abs(ifft2(imageKspaceData))); title('Inverse Fourier-kspaceData')
 
 %% Quadratic Prior 1
 algorithm = 1;
-best_alpha = 0.0001;
-best_gamma = 0.00;
+best_alpha = 0.0002;
+best_gamma = 0;
 [estimate, series] = applyGradientDescent(imageKspaceData, algorithm, best_alpha ,best_gamma, imageKspaceMask);
 display('Quadratic Prior');
 display(strcat( ['Best alpha :' num2str(best_alpha)] )); 
@@ -20,8 +20,8 @@ figure; plot(1:100, series); title('Quadratic MRF Image');
 
 %% Huber Prior 2 
 algorithm = 2;
-best_alpha = 0.000001;
-best_gamma = 0.012;
+best_alpha = 0.00001;
+best_gamma = 0.015;
 [estimate, series] = applyGradientDescent(imageKspaceData, algorithm, best_alpha , best_gamma , imageKspaceMask);
 display('Huber Prior');
 display(strcat( ['Best alpha :' num2str(best_alpha)] )); 
@@ -32,7 +32,7 @@ figure; plot(1:100, series); title('Huber MRF Image');
 %% Adaptive Prior 3
 algorithm = 3;
 best_alpha = 0.00001;
-best_gamma = 0.09;
+best_gamma = 0.08;
 [estimate, series] = applyGradientDescent(imageKspaceData, algorithm, best_alpha , best_gamma , imageKspaceMask);
 display('Adaptive Prior');
 display(strcat( ['Best alpha :' num2str(best_alpha)] )); 
